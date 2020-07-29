e928aef3cd6535d105ab9af1a4e85476a4c171e3
thienle282 <48636234+thienle282@users.noreply.github.com>
2020-07-30 11:09:02 +1200

    post build heroku
    post build heroku

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use("/posts", require("./routes/post"));
 
-app.get("/", (req, res) => {
-  res.send({ Project: "Crud app" });
-});
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
